a77961e57bf87289dda7271c80a5145b9e015d75
Abdul-Rehman-Sayed <abdulsayed454@gmail.com>
2026-03-12 18:12:20 +0530

    backend url

--- frontend/src/config/index.jsx
+++ frontend/src/config/index.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const baseURL = "http://localhost:8000";
+export const baseURL = "https://pro-network-0xna.onrender.com";
 
 export const clientServer = axios.create({
   baseURL: baseURL,
